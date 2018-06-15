8408d6b5617383fba7dcbb24079077e68c06a98e
wangzunyu <18643910385@163.com>
2018-06-15 19:49:45 +0800

    项目轮播图结束

---
+++ src/pasea/home/components/swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item in swiperList" :key="item.id">
+      <img class="img-swiper" :src="item.imgUrl">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '00001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/1c/4847ea66072c7b02.jpg_750x200_c32457fb.jpg'
+      }, {
+        id: '00002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/fc/e47aa3e1c67bbc02.jpg_750x200_0f3eecf8.jpg'
+      }, {
+        id: '00003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/f0/2a9ec1485adc7302.jpg_750x200_bc4047ed.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    width 100%
+    height 0
+    overflow hidden
+    padding-bottom 31.25%
+    .img-swiper
+      width 100%
+</style>
