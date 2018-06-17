7a6f4bb78996b3fb47f888ece1e2c073c6f211a2
wangzunyu <18643910385@163.com>
2018-06-17 21:59:40 +0800

    收缩框完成

--- src/pasea/city/city.vue
+++ src/pasea/city/city.vue
@@ -1,13 +1,18 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-sech></city-sech>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
 import CityHeader from './components/Header'
+import CitySech from './components/sech'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySech
   }
 }
 </script>
---
+++ src/pasea/city/components/sech.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="sech">
+    <input class="sech-input" type="text" placeholder="输入城市/地址">
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'CitySech'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import '~styles/varibles.styl'
+  .sech
+    height .72rem
+    background $bgColor
+    padding 0 .1rem
+    .sech-input
+      box-sizing border-box
+      padding 0 .1rem
+      width 100%
+      height .62rem
+      line-height .62rem
+      text-align center
+      border-radius .06rem
+</style>
