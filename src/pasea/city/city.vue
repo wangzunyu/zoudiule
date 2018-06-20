8d5b022e810e143f2e9b7edfeb7082dbbc1ff71e
wangzunyu <18643910385@163.com>
2018-06-20 16:32:29 +0800

    城市列表

--- src/pasea/city/city.vue
+++ src/pasea/city/city.vue
@@ -2,17 +2,23 @@
   <div>
     <city-header></city-header>
     <city-sech></city-sech>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import CityHeader from './components/Header'
 import CitySech from './components/sech'
+import CityList from './components/list'
+import CityAlphabet from './components/alphabet'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySech
+    CitySech,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
---
+++ src/pasea/city/components/alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    top 1.58rem
+    right 0
+    width .4rem
+    bottom 0
+    // background red
+    .item
+      color #ddd
+      text-align center
+</style>
