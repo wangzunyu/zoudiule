95a6fc75a3e680ad82009ca0a6e05df083100b71
wangzunyu <18643910385@163.com>
2018-06-23 15:30:53 +0800

    网页优化

--- src/pasea/home/home.vue
+++ src/pasea/home/home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/icon'
 import HomeRecommend from './components/recommend'
 import HomeWeekend from './components/weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   components: {
     HomeHeader,
@@ -25,15 +26,19 @@ export default {
   },
   data () {
     return {
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+   ...mapState(['city']) 
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -48,7 +53,14 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
